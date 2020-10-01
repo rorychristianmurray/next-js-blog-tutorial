fc0de25a976e5a5d272e28d1c2a0e3658effd8b3
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2020-10-01 14:52:10 -0400

    Set up first page with links

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -10,13 +11,12 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read my first post <Link href="/posts/first-post"><a>Here</a></Link>
         </h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function FirstPost() {
+  return(
+    <div>
+      <div>
+        hello post
+      </div>
+      <div><Link href="/">head back</Link></div>
+    </div>
+  )
+}
