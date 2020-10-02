d3633a2d774c2a793bdcad3d19f0130d437192ef
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2020-10-02 17:17:34 -0400

    Add styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,20 @@
+import Head from 'next/head'
 import Link from "next/link"
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return(
-    <div>
+    <Layout>
+      <Head>
+        <title>hello world</title>
+      </Head>
       <div>
         hello post
       </div>
-      <div><Link href="/">head back</Link></div>
-    </div>
+      
+      <div>
+        <Link href="/">head back</Link>
+      </div>
+    </Layout>
   )
 }
